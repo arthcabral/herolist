6d75b014b353c266cf745cc7f634c7c7aa41b6a8
arthcabral <ac-silva95@hotmail.com>
2021-07-23 14:45:06 -0400

    Adding the Vuelidate package to the application.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueSweetalert2 from 'vue-sweetalert2';
+import Vuelidate from 'vuelidate';
 import App from './App.vue';
 import router from './router';
 
@@ -9,6 +10,7 @@ import '../node_modules/nprogress/nprogress.css';
 Vue.config.productionTip = false;
 
 Vue.use(VueSweetalert2);
+Vue.use(Vuelidate);
 
 new Vue({
   router,
